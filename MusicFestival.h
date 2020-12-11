02e48b2861937c1e562bb2525930c9e3ff0ba78a
BogdanDidukh2003 <diduhbogdan441@gmail.com>
2020-12-11 18:53:17 +0200

    Splited project into 5 files

---
+++ MusicFestival.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "Musician.h"
+
+using namespace std;
+
+
+class MusicFestival
+{
+private:
+	int maxBudget;
+	int currentBudget;
+	vector<Musician> vectorOfMusiciansOnFestival;
+
+public:
+	MusicFestival();
+
+	MusicFestival(vector<Musician> vectorOfMusiciansOnFestival, int MaxBudget);
+
+	~MusicFestival();
+
+	int getMaxBudget();
+
+	int getCurrentBudget();
+
+	int getMusicianIndexInVector(vector<Musician> vectorOfMusiciansOnFestival, Musician musicianObject);
+
+	void addMusician(Musician musicianObject);
+
+	void deleteMusician(Musician musicianObject);
+
+	void outputMusiciansOnFestival();
+
+};
